56ad5cf07d5b780c9f164641f83d59354fc34d47
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-12-17 13:52:44 +0500

    Ftech API and add basket

---
+++ src/components/GoodList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import GoodItem from './GoodItem'
+
+const GoodList = (props) => {
+  const { goods = [], addToBasket } = props
+
+  if (!goods.length) {
+    return <h3>Nothing found</h3>
+  }
+
+  return (
+    <div className='goods'>{goods.map((item) => (
+      <GoodItem key={item.id} {...item} addToBasket={addToBasket} />
+    ))}</div>
+  );
+}
+
+export default GoodList;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const API_KEY = '7864fa1b-054b8137-050983b9-930a9535'
+const API_URL = ' https://fortniteapi.io/v1/shop?lang=en'
+
+export { API_KEY, API_URL }
